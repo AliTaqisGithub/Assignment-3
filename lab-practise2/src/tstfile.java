76f36b3fcff07e06dbae4159195317d7c65416be
AliTaqisGithub <167175434+AliTaqisGithub@users.noreply.github.com>
2024-05-07 13:39:07 +0500

    Add files via upload

---
+++ lab-practise2/src/tstfile.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class tstfile  {
+    public static void main(String[] args) {
+        FileReaderExample filereader = new FileReaderExample();
+        String lines = filereader.toString();
+        System.out.println(lines);
+
+    }
+
+}
